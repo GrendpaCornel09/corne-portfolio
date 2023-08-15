8b69e683fd0044230fca413bb66c8c7e2f438b17
Cornelius <62705112+GrendpaCornel09@users.noreply.github.com>
2023-08-15 20:35:49 +0700

    Added new project, other changes

--- script.js
+++ script.js
@@ -64,34 +64,6 @@ let nextButton = document.querySelectorAll('.next-btn')[0];
 let prevButton = document.querySelectorAll('.prev-btn')[0];
 let currentSlide = 0;
 
-// auto slide
-let repeat = function(activeClass){
-    let active = document.getElementsByClassName('active');
-
-    let i = 1;
-    var repeater = function(){
-        setTimeout(function(){
-            [...active].forEach(function(activeSlide){
-                activeSlide.classList.remove('active');
-            });
-    
-            slides[i].classList.add('active');
-            i++;
-    
-            if(slides.length == i){
-                i = 0;
-            }
-    
-            if(i >= slides.length){
-                return;
-            }
-    
-            repeater();
-        }, 5000);
-    }
-    repeater();
-}
-repeat();
 
 // show current slide
 function showSlide(index){
